4792ece318f0ab3b53868afefe249d04fbdbbd29
Amara Shein <ashein00@stuy.edu>
2019-03-06 11:44:59 -0500

    started the lab

---
+++ USACO.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class USACO{
+  // BRONZE
+  public static int bronze(String filename) throws FileNotFoundException{
+    int[][] pasture;
+    int r = 0; // number of rows
+    int c = 0; // number of columns
+    int e = 0; // final water elevation
+    int n = 0; // number of stomping directions
+
+    try{
+      File toRead = new File(filename);
+      Scanner search = new Scanner(toRead);
+
+    }
+
+
+  }
+}
